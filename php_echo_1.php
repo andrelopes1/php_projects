2e664c003ee6535ea474e75e5634c220c23870b5
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-12 18:03:21 +0100

    php

---
+++ php_echo_1.php
@@ -0,0 +1,12 @@
+<?php
+// Fill in the blanks in the code below:
+  $noun = "he";
+  $adjective = "ugly";
+  $verb = "drive";
+
+  echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+
+
+//Fix the code below and uncomment it:
+
+ echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
---
+++ php_stings_1.php
@@ -0,0 +1,21 @@
+<?php
+  $movie = "___";
+  // Add your code here:
+
+  $movie = "Open";
+
+  $old_favorite = $movie;
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+  // Add a statement here:
+  
+  $movie = "Oranges";
+  
+  echo "\nBut now my favorite is ${movie}.";
+  
+  // Add a statement below:
+
+  echo "\nActually, my old favorite was ${old_favorite}.";
+?>
+
