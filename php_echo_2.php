4042ed1e6c991160f94acd59f7c36066a6201797
André Lopes <65914110+andrelopes1@users.noreply.github.com>
2024-08-12 22:13:41 +0100

    w

---
+++ php_echo_2.php
@@ -0,0 +1,18 @@
+<?php
+  echo "I'm going on a picnic!";
+
+  $sentence = "\nI'm going on a picnic, and I'm taking apples";
+
+  echo $sentence;
+
+// Write your code below:
+
+
+$sentence .= ", bananas";
+
+echo $sentence;
+
+
+$sentence .= ", cake";
+
+echo $sentence;
---
+++ php_echo_3.php
@@ -0,0 +1,13 @@
+<?php
+/* Imagine a lot of code here */  
+  $very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order =& $very_bad_unclear_name;
+
+$order .= " and french fries.";
+
+    
+  // Don't change the line below
+  echo "\nYour order is: $very_bad_unclear_name";
