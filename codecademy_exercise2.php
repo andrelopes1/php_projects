94b55667d1579516a64e742438dd3c31927fd874
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-03 13:29:24 +0100

    Create codecademy_exercise2.php

---
+++ codecademy_exercise2.php
@@ -0,0 +1,9 @@
+//We have 82 students going on a class trip. We want to divide the students into groups of 6. Use the modulo operator to echo how many students will be left without groups. 
+
+
+<?php
+// Write your code below:
+  
+  
+  
+echo 82 % 6;
