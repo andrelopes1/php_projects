9a6ad15f9b37a51568c1035b2ded8c9eae2a4af7
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-04 15:56:56 +0100

    Create php_functions6.php

---
+++ php_functions6.php
@@ -0,0 +1,18 @@
+<?php
+// Write your code below:
+
+function increaseEnthusiasm($name){
+  return $name . "!";
+}
+
+// Testing the function
+echo increaseEnthusiasm("hi"); // This should output "hi!"
+
+function repeatThreeTimes($str) {
+  return $str . $str . $str;
+}
+
+// Test the repeatThreeTimes function with a string of your choice
+echo repeatThreeTimes("Hello"); // This will output: HelloHelloHello
+
+?>
