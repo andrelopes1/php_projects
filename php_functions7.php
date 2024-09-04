b0b94cd721dd1cbd087d33545f70da5c06497035
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-04 16:06:42 +0100

    Create php_functions7.php

---
+++ php_functions7.php
@@ -0,0 +1,18 @@
+<?php
+// Write your code below:
+
+function calculateArea($height, $width){
+  return $height * $width ;
+
+}
+
+
+echo calculateArea(10, 10);
+
+
+function calculateVolume($height, $width, $lenght){
+  return "\n" . $height * $width * $lenght;
+
+}
+
+echo calculateVolume(10, 10, 10);
