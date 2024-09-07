b6a421e4940069842ac766901f0d816127633321
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-07 14:08:01 +0100

    Create php_functions11.php

---
+++ php_functions11.php
@@ -0,0 +1,22 @@
+<?php
+namespace Codecademy;
+
+$first = "Welcome to the magical world of built-in functions.";
+  
+$second = 82137012983; 
+
+//Write your code below:
+
+echo gettype($first);
+
+echo "\n";
+
+echo gettype($second);
+
+echo "\n";
+
+echo var_dump($first);
+
+echo "\n";
+
+echo var_dump($second);
