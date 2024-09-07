d5ce5c384bb8aeed13dcb7630442dd58a875762c
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-07 14:14:07 +0100

    Create php_functions.12.php

---
+++ php_functions.12.php
@@ -0,0 +1,33 @@
+<?php
+namespace Codecademy;
+
+/*1.
+
+Use echo and the strrev() function to print the following string reversed: ".pu ti peeK .taerg gniod er'uoY"
+Checkpoint 2 Passed
+
+2.
+
+Use echo and the strtolower() function to print the following string with all lowercase letters: "SOON, tHiS WILL Look NoRmAL."
+Checkpoint 3 Passed
+
+3.
+
+Use echo and the str_repeat() function to print the string "\nThere's no place like home.\n" three times.
+*/
+
+
+
+// Write your code below:
+
+echo strrev("\n.pu ti peeK .taerg gniod er'uoY\n");
+
+echo strtolower("\nSOON, tHiS WILL Look NoRmAL.\n");
+
+echo str_repeat("\nThere's no place like home.\n", 3);
+
+
+
+
+
+
