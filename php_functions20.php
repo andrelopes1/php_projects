74aa8a4ce9ff279fa84d243424712ef1c7a8fd7e
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-09 11:58:00 +0100

    Update php_functions20.php

--- php_functions20.php
+++ php_functions20.php
@@ -1,6 +1,29 @@
 <?php
-namespace Codecademy;
 
+/*
+1.
+
+Write a function agreeOrDisagree() that takes in two strings, and returns "You agree!" if the two strings are the same and "You disagree!" if the two strings are different.
+Checkpoint 2 Passed
+
+2.
+
+Test your function! Invoke it once with a value that will result in the if block running and once with a value that won’t.
+Checkpoint 3 Passed
+
+3.
+
+You’re going to write a function to check if it’s time for a user to renew their subscription.
+
+Write a function checkRenewalMonth() that takes in a user’s renewal month as a string (e.g. "January").
+
+Your function should get the current month using the PHP built-in date() function (see the hint for help with this). It should compare the current month to the renewal month passed in. If the renewal month is not the current month, the function should return the string "Welcome!". Otherwise it should return the string "Time to renew".
+Checkpoint 4 Passed
+
+4.
+
+Test your function! Invoke it once with a value that will result in the if block running and once with a value that won’t.
+*/
 function agreeOrDisagree($color1, $color2){
   if ($color1 === $color2){
     return "You agree!";
