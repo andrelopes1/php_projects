dd54ce9934aa4e57873f1af3b8c6cc6c0859e0dd
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-09 12:37:41 +0100

    Update functions25.php

--- functions25.php
+++ functions25.php
@@ -1,6 +1,15 @@
 <?php
-namespace Codecademy;
 
+/*
+1.
+
+Since it’s hard to keep track of what’s truthy or falsy in PHP, let’s write a function to check for us! Write a function truthyOrFalsy() that takes in any value and returns the string "True" if that value is truthy and the string "False" if that value is falsy.
+Checkpoint 2 Passed
+
+2.
+
+Test your function! Invoke your function at least once with a truthy value and at least once with a falsy value. Be sure to use echo to print the results to the terminal.
+*/
 
 function truthyOrFalsy($value) {
     return $value ? "True" : "False";
