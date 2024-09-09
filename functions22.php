c300475c32474c2b5f6ada969b2c9feda6d0db8c
Andre Lopes <andrelopesempresa@gmail.com>
2024-09-09 12:13:58 +0100

    Update functions22.php

--- functions22.php
+++ functions22.php
@@ -1,5 +1,25 @@
 <?php
-namespace Codecademy;
+/*
+1.
+
+The U.S. government has multiple classifications of air quality each symbolized by a color:
+
+    The color "green" indicates the air quality is "good".
+    The color "yellow" indicates the air quality is "moderate".
+    The color "orange" indicates the air quality is "unhealthy for sensitive groups".
+    The color "red" indicates the air quality is "unhealthy".
+    The color "purple" indicates the air quality is "very unhealthy".
+    And the color "maroon" indicates the air quality is "hazardous".
+
+Write a function, airQuality(), that takes in a color as a string, and prints the corresponding air quality.
+
+Your function should use a switch statement. You should provide a default string of "invalid color" for any input aside from the six colors listed.
+Checkpoint 2 Passed
+
+2.
+
+Test your function with several inputs to make sure it’s working as expected.
+*/
 
 function airQuality($color) {
     switch ($color) {
